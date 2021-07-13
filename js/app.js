996dc90e49d211f3aa558af8d15eb1fe669404a6
LuciLua <luci.lua81@gmail.com>
2021-07-13 19:54:36 -0300

    :cactus: padding content and menu border-bottom ON

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 function menu(){
     var toggleMenu = document.querySelector('header')
     toggleMenu.classList.toggle('header-flex')
-    document.body.classList.toggle('body-overflow-none')
+    // document.body.classList.toggle('body-overflow-none')
+    $(window).scrollTop(0);
+    $(window).bind('scroll', setTopo);
 }

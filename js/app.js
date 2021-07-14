99c6840346ec0d436f64194a37303395129983e0
LuciLua <luci.lua81@gmail.com>
2021-07-13 22:29:35 -0300

    :cactus: waves

--- js/app.js
+++ js/app.js
@@ -2,6 +2,4 @@ function menu(){
     var toggleMenu = document.querySelector('header')
     toggleMenu.classList.toggle('header-flex')
     // document.body.classList.toggle('body-overflow-none')
-    $(window).scrollTop(0);
-    $(window).bind('scroll', setTopo);
 }
